726c37458dd80c7c1e2bda1f522e67228251a563
Bean38200 <benac38200@gmail.com>
2026-02-06 11:37:19 -0700

    Added the deathzone and deathzone script

---
+++ Assignment01_GMPR2512/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoint;
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.CompareTag("Ball"))
+        {
+            GameObject ball = collider2D.gameObject;
+            //Wait 2 seconds before doing something
+            StartCoroutine(RespawnBall(ball));
+        }
+
+    }
+
+    //StartCoroutine must be passed a method that returns an IEnumerator
+    private IEnumerator RespawnBall(GameObject ball)
+    {
+        Debug.Log("That's crazy... but it just might work!");
+        yield return new WaitForSeconds(2);
+        Rigidbody2D ballRB = ball.GetComponent<Rigidbody2D>();
+        ballRB.linearVelocity = Vector2.zero;
+        ballRB.angularVelocity = 0;
+        ball.transform.position = _spawnPoint.position;
+        Debug.Log("That move just cost us 50 children.");
+    }
+}
+
